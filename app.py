55ad60adbbfe92958098421d51aba33391f862ee
Lucas Nobre Barbosa <lucasbnobre18@gmail.com>
2023-02-15 20:41:25 -0300

    First Commit

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template, url_for
+
 app = Flask(__name__)
 
 digimons = [
     {
-        'nome': 'Lucas Nobre Barbosa',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Ádria Leão de Moura',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
+        'nome': 'Agumon',
+        'nivel': 'Criança',
+        'Atributo': 'Vacina',
+        'perfil': """É um Digimon Réptil que desenvolveu bipedismo e tem a aparência de um pequeno dinossauro. Por 
+        ainda estar a caminho da fase adulta, o seu poder é reduzido, no entanto, não entende o medo, por isso a sua 
+        personalidade é muito feroz. Cresceram-lhe afiadas e robustas garras em ambas as mãos e pés, cujo poder 
+        demonstra em batalha. Também prediz a evolução para um grande e poderoso Digimon. O seu movimento especial é 
+        cuspir fogo da boca para atacar o oponente (Baby Flame).[6]"""
     }
 ]
 
@@ -20,12 +19,12 @@ digimons = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    return "Hello World"
 
 
-@app.route("/about")
+@app.route("/digimons")
 def about():
-    return render_template('about.html', title='About')
+    return render_template('digimons.html', digimons=digimons)
 
 
 if __name__ == '__main__':
